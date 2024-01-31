b6b9affd1a1c13be63f14cd03367374589dc8ecf
kridots <kritesh.ojha@dotsquares.com>
2024-01-31 19:36:18 +0530

    Forget password functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('register/', views.register,name='register'),
     path('login/', views.login,name='login'),
     path('logout/', views.logout_view,name='logout'),
+    path('password-reset/',auth_view.PasswordResetView.as_view(template_name='auth/password_reset_form.html'), name='password-reset'),
+    path('password-reset-done/',auth_view.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/',auth_view.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -141,9 +141,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #SMTP config
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_USE_TLS = True
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL=env("EMAIL_HOST_USER")
